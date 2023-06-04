2a77aa5de5202cd335a85ee623ba249e515ce61a
alief azies <alief.azies@phincon.com>
2023-06-04 11:33:50 +0700

    create more storyboard for tab bar controller

---
+++ FoodDelivery/Module/Offers/OffersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OffersViewController.swift
+//  FoodDelivery
+//
+//  Created by PT Phincon on 26/05/23.
+//
+
+import UIKit
+
+class OffersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
